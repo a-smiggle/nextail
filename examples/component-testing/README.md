10ef3e0d7a8474f459b1fee58054e0dec65a1a78
Anthony Smigielski <anthony.smigielski@protonmail.com>
2022-04-26 14:21:55 +0800

    Update to wanted structure
    Removes unwanted packages and adds wanted repo structure.
    
    Anthony Smigielski

---
+++ examples/component-testing/README.md
@@ -0,0 +1,3 @@
+# Compoent Testing
+
+A blank Next.JS example that is used for importing and testing components during development.
---
+++ websites/landing-page/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const Home: NextPage = () => (
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+);
+
+export default Home;
