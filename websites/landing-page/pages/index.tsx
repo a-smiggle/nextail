4b19cc30948d24f453ac333e3c015ede6fd4c839
Anthony Smigielski <anthony.smigielski@protonmail.com>
2022-05-19 08:55:19 +0800

    landing-page: 0.4.0
    Update and add to index.
    - Customisation added.
    
    a-smiggle

--- websites/landing-page/pages/index.tsx
+++ websites/landing-page/pages/index.tsx
@@ -19,38 +19,38 @@ const Home: NextPage = () => {
     <>
       <Layout>
         <div
-          className="h-full w-fit bg-white bg-cover bg-no-repeat dark:bg-emerald-900 md:w-screen"
+          className="h-full w-full bg-white bg-cover bg-no-repeat dark:bg-emerald-900"
           style={{ backgroundImage: 'url(/EmeraldDivider.svg)' }}
         >
           <div className="w-full pt-6 text-center md:pt-24">
-            <div className="mx-auto h-fit w-fit rounded p-6 shadow-lg dark:bg-emerald-500/30">
-              <h1 className="text-center text-4xl font-bold text-white md:text-6xl">
+            <div className="w-full flex-col justify-center rounded shadow-lg dark:bg-emerald-500/30 md:p-6">
+              <h1 className="text-center text-6xl font-bold text-white md:text-4xl">
                 Easy to use.
               </h1>
-              <h1 className="text-4xl font-bold text-white md:text-6xl">
+              <h1 className="text-6xl font-bold text-white md:text-4xl">
                 Easy to configure.
               </h1>
-              <h1 className="text-4xl font-bold text-white md:text-6xl">
+              <h1 className="text-6xl font-bold text-white md:text-4xl">
                 Easy to customise.
               </h1>
               <br></br>
-              <p className="pt-6 text-lg font-medium text-black">
+              <p className="pt-6 text-2xl font-medium text-white md:text-lg">
                 A collection of React components built with TailwindCSS. Styled
                 and semistyled options ready to be customised.
               </p>
             </div>
           </div>
-          <div className="grid gap-4 p-4 md:grid-cols-3">
-            <div className="col-span-1 w-full rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20">
-              <h1 className="pb-6 text-2xl font-bold text-white">
+          <div className="grid gap-4 md:grid-cols-3 md:p-4">
+            <div className="col-span-1 w-screen rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20 md:w-full">
+              <h1 className="pb-6 text-4xl font-bold text-white md:text-2xl">
                 Installation
               </h1>
               <div className="flex justify-center">
                 <CodeSnippet data="npm i @nextail/core @nextail/providers" />
               </div>
             </div>
-            <div className="col-span-2 w-full rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20">
-              <h1 className="pb-2 text-2xl font-bold text-white">
+            <div className="col-span-1 w-screen rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20 md:col-span-2 md:w-full">
+              <h1 className="pb-2 text-4xl font-bold text-white md:text-2xl">
                 Configuration
               </h1>
               <p className="text-lg font-medium text-black">
@@ -72,8 +72,13 @@ const Home: NextPage = () => {
 };`}
               ></CodeSnippet>
             </div>
-            <div className="col-span-1 w-full rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20 md:col-span-3">
-              <h1 className="pb-2 text-2xl font-bold text-white">Use</h1>
+            <div className="col-span-1 w-screen rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20 md:col-span-2 md:w-full">
+              <h1 className="pb-2 text-4xl font-bold text-white md:text-2xl">
+                Use
+              </h1>
+              <p className="text-lg font-medium text-black">
+                Example of Next.JS page file found in pages folder.
+              </p>
               <CodeSnippet
                 data={`import Button, {
   ErrorButton,
@@ -120,7 +125,7 @@ const ButtonPage: NextPage = () => {
 export default ButtonPage;
 `}
               ></CodeSnippet>
-              <div className="flex justify-between p-2">
+              <div className="space-x-2 space-y-2 md:flex md:justify-between md:p-2">
                 <Button>Hey</Button>
                 <ErrorButton>Error</ErrorButton>
                 <ErrorOutlineButton>Error</ErrorOutlineButton>
@@ -130,6 +135,69 @@ export default ButtonPage;
                 <SuccessOutlineButton>Success</SuccessOutlineButton>
                 <WarningButton>Warning</WarningButton>
                 <WarningOutlineButton>Warning</WarningOutlineButton>
+              </div>
+            </div>
+            <div className="col-span-1 w-screen rounded bg-emerald-500/50 p-6 shadow-lg dark:bg-emerald-500/20 md:col-span-1 md:w-full">
+              <h1 className="pb-2 text-4xl font-bold text-white md:text-2xl">
+                Customise
+              </h1>
+              <p className="text-lg font-medium text-black">
+                Easily edit individual class variables or overide with complete
+                control.
+              </p>
+              <CodeSnippet
+                data={`import Button from '@nextail/core/Button
+import { NextPage } from 'next';
+import React from 'react';
+
+const ButtonPage: NextPage = () => {
+  return (
+    <div className="h-screen w-full bg-slate-400">
+      <div className="flex justify-between p-2">
+        <Button
+        mainStylings={{
+          border: { borderRadius: 'rounded-full' },
+          background: { backgroundColor: 'bg-emerald-500' },
+          text: { textColor: 'text-white' },
+          filter: { invert: 'hover:invert' },
+        }}
+        >
+          Hey
+        </Button>
+        <Button
+          mainStylings={{
+            className:
+              'px-6 py-2 text-center uppercase leading-light font-medium text-xs shadow-md rounded-full bg-emerald-500 inline-block text-white hover:invert',
+          }}
+        >
+          Hey
+        </Button>
+      </div>
+    </div>
+    );
+  };
+  
+  export default ButtonPage;`}
+              ></CodeSnippet>
+              <div className="space-x-2 pt-2">
+                <Button
+                  mainStylings={{
+                    border: { borderRadius: 'rounded-full' },
+                    background: { backgroundColor: 'bg-emerald-500' },
+                    text: { textColor: 'text-white' },
+                    filter: { invert: 'hover:invert' },
+                  }}
+                >
+                  Hey
+                </Button>
+                <Button
+                  mainStylings={{
+                    className:
+                      'px-6 py-2 text-center uppercase leading-light font-medium text-xs shadow-md rounded-full bg-emerald-500 inline-block text-white hover:invert',
+                  }}
+                >
+                  Hey
+                </Button>
               </div>
             </div>
           </div>
