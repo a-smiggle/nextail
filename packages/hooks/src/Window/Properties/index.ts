676dbc6606ce19e9ea0908f9d743913c06cf2a46
Anthony Smigielski <anthony.smigielski@protonmail.com>
2022-06-02 20:10:44 +0800

    hooks: 0.46.0
    Add index file for all Window/Properties hooks.
    Exports all within an object.
    
    a-smiggle
    
    a-smiggle

---
+++ packages/hooks/src/Window/Properties/index.ts
@@ -0,0 +1,87 @@
+import useCaches from './UseCaches';
+import useClientInformation from './UseClientInformation';
+import useClosed from './UseClosed';
+import useConsole from './UseConsole';
+import useCrypto from './UseCrypto/UseCrypto';
+import useCustomElements from './UseCustomElements';
+import useDevicePixelRatio from './UseDevicePixelRatio';
+import useDocument from './UseDocument';
+import useFrameElement from './UseFrameElement';
+import useFrames from './UseFrames';
+import useHistory from './UseHistory';
+import useIndexDB from './UseIndexDB';
+import useInnerHeight from './UseInnerHeight';
+import useInnerWidth from './UseInnerWidth';
+import useLength from './UseLength';
+import useLocalStorage from './UseLocalStorage';
+import useLocation from './UseLocation';
+import useLocationbar from './UseLocationbar';
+import useMenubar from './UseMenubar';
+import useName from './UseName';
+import useNavigator from './UseNavigator';
+import useOpener from './UseOpener';
+import useOrigin from './UseOrigin';
+import useOuterHeight from './UseOuterHeight';
+import useOuterWidth from './UseOuterWidth';
+import usePageXOffset from './UsePageXOffset';
+import usePageYOffset from './UsePageYOffset';
+import useParent from './UseParent';
+import usePerformance from './UsePerformance';
+import usePersonalbar from './UsePersonalbar';
+import useScreen from './UseScreen';
+import useScreenLeft from './UseScreenLeft';
+import useScreenTop from './UseScreenTop';
+import useScrollbars from './UseScrollbars';
+import useScrollX from './UseScrollX';
+import useScrollY from './UseScrollY';
+import useSelf from './UseSelf';
+import useSessionStorage from './UseSessionStorage';
+import useSpeechSynthesis from './UseSpeechSynthesis';
+import useStatusbar from './UseStatusbar';
+import useToolbar from './UseToolbar';
+import useTop from './UseTop';
+
+export {
+  useCaches,
+  useClientInformation,
+  useClosed,
+  useConsole,
+  useCrypto,
+  useCustomElements,
+  useDevicePixelRatio,
+  useDocument,
+  useFrameElement,
+  useFrames,
+  useHistory,
+  useIndexDB,
+  useInnerHeight,
+  useInnerWidth,
+  useLength,
+  useLocalStorage,
+  useLocation,
+  useLocationbar,
+  useMenubar,
+  useName,
+  useNavigator,
+  useOpener,
+  useOrigin,
+  useOuterHeight,
+  useOuterWidth,
+  usePageXOffset,
+  usePageYOffset,
+  useParent,
+  usePerformance,
+  usePersonalbar,
+  useScreen,
+  useScreenLeft,
+  useScreenTop,
+  useScrollbars,
+  useScrollX,
+  useScrollY,
+  useSelf,
+  useSessionStorage,
+  useSpeechSynthesis,
+  useStatusbar,
+  useToolbar,
+  useTop,
+};
