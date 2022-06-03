48c53a7fa8be93fc95ae81a13ba3e3daaf21c64c
Anthony Smigielski <anthony.smigielski@protonmail.com>
2022-06-03 09:56:08 +0800

    core: 0.6.15
    Update progress.
    Allows pre-styled to be customised.
    
    a-smiggle

--- packages/core/src/Progress/index.tsx
+++ packages/core/src/Progress/index.tsx
@@ -1,35 +1,79 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import Progress from './Progress';
 import { ProgressProps } from './types';
 
-const SuccessProgress = (props: ProgressProps) => (
-  <Progress
-    progress={props.progress}
-    mainStylings={{ background: { backgroundColor: 'bg-green-500' } }}
-  />
-);
+const SuccessProgress = (props: ProgressProps): ReactElement => {
+  const mainStylings = props.mainStylings ? props.mainStylings : {};
+  if (mainStylings) {
+    if (!mainStylings.background) mainStylings.background = {};
+    mainStylings.background.backgroundColor = props.mainStylings?.background
+      ?.backgroundColor
+      ? props.mainStylings?.background?.backgroundColor
+      : 'bg-green-500';
+  }
+  return (
+    <Progress
+      progress={props.progress}
+      mainStylings={mainStylings}
+      backgroundStylings={props.backgroundStylings}
+    />
+  );
+};
 
-const WarningProgress = (props: ProgressProps) => (
-  <Progress
-    progress={props.progress}
-    mainStylings={{ background: { backgroundColor: 'bg-yellow-500' } }}
-  />
-);
+const WarningProgress = (props: ProgressProps): ReactElement => {
+  const mainStylings = props.mainStylings ? props.mainStylings : {};
+  if (mainStylings) {
+    if (!mainStylings.background) mainStylings.background = {};
+    mainStylings.background.backgroundColor = props.mainStylings?.background
+      ?.backgroundColor
+      ? props.mainStylings?.background?.backgroundColor
+      : 'bg-yellow-500';
+  }
+  return (
+    <Progress
+      progress={props.progress}
+      mainStylings={mainStylings}
+      backgroundStylings={props.backgroundStylings}
+    />
+  );
+};
 
-const ErrorProgress = (props: ProgressProps) => (
-  <Progress
-    progress={props.progress}
-    mainStylings={{ background: { backgroundColor: 'bg-red-500' } }}
-  />
-);
+const ErrorProgress = (props: ProgressProps): ReactElement => {
+  const mainStylings = props.mainStylings ? props.mainStylings : {};
+  if (mainStylings) {
+    if (!mainStylings.background) mainStylings.background = {};
+    mainStylings.background.backgroundColor = props.mainStylings?.background
+      ?.backgroundColor
+      ? props.mainStylings?.background?.backgroundColor
+      : 'bg-red-500';
+  }
+  return (
+    <Progress
+      progress={props.progress}
+      mainStylings={mainStylings}
+      backgroundStylings={props.backgroundStylings}
+    />
+  );
+};
 
-const InfoProgress = (props: ProgressProps) => (
-  <Progress
-    progress={props.progress}
-    mainStylings={{ background: { backgroundColor: 'bg-sky-300' } }}
-  />
-);
+const InfoProgress = (props: ProgressProps): ReactElement => {
+  const mainStylings = props.mainStylings ? props.mainStylings : {};
+  if (mainStylings) {
+    if (!mainStylings.background) mainStylings.background = {};
+    mainStylings.background.backgroundColor = props.mainStylings?.background
+      ?.backgroundColor
+      ? props.mainStylings?.background?.backgroundColor
+      : 'bg-sky-500';
+  }
+  return (
+    <Progress
+      progress={props.progress}
+      mainStylings={mainStylings}
+      backgroundStylings={props.backgroundStylings}
+    />
+  );
+};
 
 export { ErrorProgress, InfoProgress, SuccessProgress, WarningProgress };
 export default Progress;
