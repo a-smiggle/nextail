4764cd5bea8620bd2ead15705d9cc6b02a656e5a
Anthony Smigielski <anthony.smigielski@protonmail.com>
2022-06-03 08:22:16 +0800

    core: 0.6.6
    Update badge to allow customisation.
    
    a-smiggle

--- packages/core/src/Badge/index.tsx
+++ packages/core/src/Badge/index.tsx
@@ -1,18 +1,29 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, ReactElement } from 'react';
 
 import Badge from './Badge';
 import { BadgeProps } from './types';
 
-const PillBadge = (props: PropsWithChildren<BadgeProps>) => (
-  <Badge mainStylings={{ border: { borderRadius: 'rounded-full' } }}>
-    {props.children}
-  </Badge>
-);
-const RoundedBadge = (props: PropsWithChildren<BadgeProps>) => (
-  <Badge mainStylings={{ border: { borderRadius: 'rounded' } }}>
-    {props.children}
-  </Badge>
-);
+const PillBadge = (props: PropsWithChildren<BadgeProps>): ReactElement => {
+  const mainStylings = props.mainStylings ? props.mainStylings : {};
+  if (mainStylings) {
+    if (!mainStylings.border) mainStylings.border = {};
+    mainStylings.border.borderRadius = props.mainStylings?.border?.borderRadius
+      ? props.mainStylings?.border?.borderRadius
+      : 'rounded-full';
+  }
+
+  return <Badge mainStylings={mainStylings}>{props.children}</Badge>;
+};
+const RoundedBadge = (props: PropsWithChildren<BadgeProps>): ReactElement => {
+  const mainStylings = props.mainStylings ? props.mainStylings : {};
+  if (mainStylings) {
+    if (!mainStylings.border) mainStylings.border = {};
+    mainStylings.border.borderRadius = props.mainStylings?.border?.borderRadius
+      ? props.mainStylings?.border?.borderRadius
+      : 'rounded';
+  }
+  return <Badge mainStylings={mainStylings}>{props.children}</Badge>;
+};
 
 export { PillBadge, RoundedBadge };
 export default Badge;
